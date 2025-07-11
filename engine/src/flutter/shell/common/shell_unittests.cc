4455f1d60d0f109cc87477be82965a3ceb927d95
Jason Simmons <jason-simmons@users.noreply.github.com>
2025-07-11 09:48:09 -0700

    Detach the resource context from the IO thread only if the shell's IO manager is no longer being used by any other spawned shells (#171554)
    The shell had been calling PlatformView::ReleaseResourceContext every
    time a shell instance shuts down in order to clear the EGL context from
    the IO thread.
    But there may be other spawned shells that are also using the same IO
    manager and IO task runner. If the task that calls
    ReleaseResourceContext runs after the other shells call
    CreateResourceContext, then the IO thread will be left without an EGL
    context. That will cause errors in operations like image decoding that
    need to invoke OpenGL ES APIs on the IO thread.
    
    See https://github.com/flutter/flutter/issues/171213

--- engine/src/flutter/shell/common/shell_unittests.cc
+++ engine/src/flutter/shell/common/shell_unittests.cc
@@ -238,6 +238,7 @@ class TestPlatformView : public PlatformView {
   TestPlatformView(Shell& shell, const TaskRunners& task_runners)
       : PlatformView(shell, task_runners) {}
   MOCK_METHOD(std::unique_ptr<Surface>, CreateRenderingSurface, (), (override));
+  MOCK_METHOD(void, ReleaseResourceContext, (), (const, override));
 };
 
 class MockPlatformMessageHandler : public PlatformMessageHandler {
@@ -5070,6 +5071,86 @@ TEST_F(ShellTest, ProvidesNullEngineId) {
   latch.Wait();
   ASSERT_EQ(reported_handle, std::nullopt);
   DestroyShell(std::move(shell), task_runners);
+}
+
+TEST_F(ShellTest, MergeUIAndPlatformThreadsAfterLaunch) {
+  Settings settings = CreateSettingsForFixture();
+  settings.merged_platform_ui_thread =
+      Settings::MergedPlatformUIThread::kMergeAfterLaunch;
+  ThreadHost thread_host(ThreadHost::ThreadHostConfig(
+      "io.flutter.test." + GetCurrentTestName() + ".",
+      ThreadHost::Type::kPlatform | ThreadHost::Type::kRaster |
+          ThreadHost::Type::kIo | ThreadHost::Type::kUi));
+  TaskRunners task_runners("test", thread_host.platform_thread->GetTaskRunner(),
+                           thread_host.raster_thread->GetTaskRunner(),
+                           thread_host.ui_thread->GetTaskRunner(),
+                           thread_host.io_thread->GetTaskRunner());
+
+  std::unique_ptr<Shell> shell = CreateShell(settings, task_runners);
+  ASSERT_TRUE(shell);
+
+  ASSERT_FALSE(fml::TaskRunnerChecker::RunsOnTheSameThread(
+      task_runners.GetUITaskRunner()->GetTaskQueueId(),
+      task_runners.GetPlatformTaskRunner()->GetTaskQueueId()));
+
+  fml::AutoResetWaitableEvent latch;
+  AddNativeCallback(
+      "NotifyNative", CREATE_NATIVE_ENTRY([&](auto args) {
+        ASSERT_TRUE(
+            task_runners.GetPlatformTaskRunner()->RunsTasksOnCurrentThread());
+        latch.Signal();
+      }));
+
+  auto configuration = RunConfiguration::InferFromSettings(settings);
+  configuration.SetEntrypoint("mainNotifyNative");
+  RunEngine(shell.get(), std::move(configuration));
+
+  latch.Wait();
+
+  ASSERT_TRUE(fml::TaskRunnerChecker::RunsOnTheSameThread(
+      task_runners.GetUITaskRunner()->GetTaskQueueId(),
+      task_runners.GetPlatformTaskRunner()->GetTaskQueueId()));
+
+  DestroyShell(std::move(shell), task_runners);
+}
+
+TEST_F(ShellTest, ReleaseResourceContextWhenIOManagerIsDeleted) {
+  TaskRunners task_runners = GetTaskRunnersForFixture();
+  auto settings = CreateSettingsForFixture();
+  bool called_release_resource_context = false;
+  Shell::CreateCallback<PlatformView> platform_view_create_callback =
+      [task_runners, &called_release_resource_context](flutter::Shell& shell) {
+        auto result = std::make_unique<::testing::NiceMock<TestPlatformView>>(
+            shell, task_runners);
+        ON_CALL(*result, ReleaseResourceContext())
+            .WillByDefault(
+                ::testing::Assign(&called_release_resource_context, true));
+        return result;
+      };
+
+  auto parent_shell = CreateShell({
+      .settings = settings,
+      .task_runners = task_runners,
+      .platform_view_create_callback = platform_view_create_callback,
+  });
+
+  std::unique_ptr<Shell> child_shell;
+  PostSync(
+      parent_shell->GetTaskRunners().GetPlatformTaskRunner(),
+      [&parent_shell, &settings, &child_shell, platform_view_create_callback] {
+        auto configuration = RunConfiguration::InferFromSettings(settings);
+        configuration.SetEntrypoint("emptyMain");
+        auto child = parent_shell->Spawn(
+            std::move(configuration), "", platform_view_create_callback,
+            [](Shell& shell) { return std::make_unique<Rasterizer>(shell); });
+        child_shell = std::move(child);
+      });
+
+  DestroyShell(std::move(parent_shell), task_runners);
+  ASSERT_FALSE(called_release_resource_context);
+
+  DestroyShell(std::move(child_shell), task_runners);
+  ASSERT_TRUE(called_release_resource_context);
 }
 
 }  // namespace testing
